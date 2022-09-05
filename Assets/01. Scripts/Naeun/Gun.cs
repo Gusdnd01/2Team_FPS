a26f668a3129f191cf411031d301a61cd4cdd329
NaeunKk <98926630+NaeunKk@users.noreply.github.com>
2022-09-06 01:15:11 +0900

    재장전(진행 중)

--- Assets/01. Scripts/Naeun/Gun.cs
+++ Assets/01. Scripts/Naeun/Gun.cs
@@ -6,10 +6,15 @@ public class Gun : Weapon
 {
     [SerializeField] private LayerMask layer;
     WeaponDataSo data;
+    public WeaponDataSo Data { get { return data; } }
+    private int curCapacity;
 
     void Start()
     {
-
+        data.bulletCapacity = 12;
+        data.attackDelay = 0.75f;
+        curCapacity = data.bulletCapacity;
+        StartCoroutine(Shooting());
     }
 
     void Update()
@@ -22,13 +27,26 @@ public class Gun : Weapon
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
-            hit.transform.GetComponent<IDamaged>();
+            // hit.transform.GetComponent<IDamaged>();
         }
     }
+    IEnumerator Shooting()
+    {
+        yield return new WaitForSeconds(data.attackDelay);
+        while (true)
+        {
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+            LeftClick();
+            curCapacity--;
+            Debug.DrawRay(firePos.position, firePos.forward * 15, Color.yellow);
+        }
+    }
+
     public override void RightClick()
     {
 
     }
+
     public override void PressR()
     {
 
--- Assets/01. Scripts/Naeun/Weapon.cs
+++ Assets/01. Scripts/Naeun/Weapon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
+    [SerializeField] protected Transform firePos;
     public abstract void LeftClick();
     public abstract void RightClick();
     public abstract void PressR();
